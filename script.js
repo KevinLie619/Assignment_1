eba764edc095b7c6cbfbd8a97952347788921640
Kevin Lai <kevinlie619@gmail.com>
2017-02-09 21:01:28 -0800

    Assignment 1 - Kevin Lai
    FEWD 2

---
+++ script.js
@@ -0,0 +1,117 @@
+//Step1
+var someMonth;
+function theMonth 		
+currentMonth 		
+var summerMonth; 		
+mylibraryaFunction 
+//Step2 
+//numeric expression 
+3.9
+//literal expression 
+"hello world!"
+//boolean expression
+false 
+//null literal expression
+null
+//step 3 
+var anExpression = 3 * (4 / 5) + 6;
+var aSecondExpression = Math.PI * radius * radius;
+//step 4 
+var firstName,
+var lastName,
+var Cddress
+var City, 
+var State, 
+var zipCode.
+var yourAge, 
+var referrelSource, 
+var mayWecontactYou
+//step 5
+var firstName=Kevin
+var lastName=Lai
+var yourAge=28
+//step 6 
+//step 7
+car = "Chevy";
+var car;
+console.log(car);
+//step 8 
+valid 
+//step 9
+var x = 200;
+var y = null;
+console.log(x);console.log(y);
+//step 10 
+typeof "John" 
+typeof 3.14
+typeof false 
+typeof null
+typeof mycar
+//step 11
+var x=hello
+var y=zac
+window.console.log(x+""+y)
+//step 12
+var x=hello
+var y=kevin
+window.console.log(x+""+y)
+//step 13 
+var x=hello
+var y=kevin
+var z= javascript
+window.console.log(x+""+y"welcome to"+z"class")
+//step 14 
+window.console.log(Hello Kevin\nWelcome to Javascript Class)
+//step 15
+function myFunction() {
+    var person = prompt("Please enter your name", );
+    
+    if (person != null) {
+        document.getElementById("demo").innerHTML =
+        "Hello " + person + "! How are you today?";
+    }
+}
+//step 16 
+function myFunction() {
+    var class = prompt("Please enter your class", );
+    
+    if (class != null) {
+        document.getElementById("demo").innerHTML =
+        "You are in " + class + "! How are you today?";
+    }
+}
+//step 17 
+var x=10
+var y=20
+window.console.log(x+y);
+//step 18 
+var x=20;
+window.console.log(x+20);
+//step 19 
+var x=20 
+window.console.log(x*5);
+//step 20 
+var x= 1
+var y=20/3
+window.console.log(y+x);
+//step21
+var x=1
+var y=1
+window.console.log(x==y);
+//step22
+var x=1
+var y=1
+window.console.log(x>y)
+//step 23 
+document.getElementById("demo").innerHTML = 
+typeof "john";
+typeof 3.14;
+
+//step 24
+var cars = ["Saab", "Volvo", "BMW"];
+
+document.getElementById("demo").innerHTML = (cars instanceof Object);
+//step 25
+var cars = ["Saab", "Volvo", "BMW"];
+
+document.getElementById("demo").innerHTML = (cars instanceof string);
